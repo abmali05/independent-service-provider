c950ca6f58b74ead8da0da651f143512a3462d77
abmali05 <bestofhonest@gmail.com>
2022-04-18 13:33:20 +0600

    about

--- src/App.js
+++ src/App.js
@@ -20,7 +20,8 @@ function App() {
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/home" element={<Home></Home>}></Route>
         <Route path="/login" element={<Login></Login>}></Route>
-        <Route path="/about" element={<About></About>}></Route>
+        <Route path="/login" element={<About></About>}></Route>
+        {/* <Route path="/about" element={<About></About>}></Route> */}
         <Route path="/register" element={<Register></Register>}></Route>
         <Route path='/service/:serviceId' element={<ServiceDetail></ServiceDetail>}></Route>
         <Route path="/checkout" element={
