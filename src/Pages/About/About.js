33d67e616006dbb0c48f130fac33a5f5206ff96d
abmali05 <bestofhonest@gmail.com>
2022-04-18 19:06:58 +0600

    about added

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            <h2 className='text-center'>This is About</h2>
+        <div className='container mx-auto w-50'>
+            <div className='row mt-5 text-justify'>
+                <h5 className='text-center'>Allah is almighty</h5>
+                <p>Salam. I am Abu Bakar Mohammad Ali. Like others students I have also read an essay 'Aim in Life'. But to gain success in life depends on some subjects. Such as honesty, industry, punctuality etc. Only by dint of hard work one can not gain success in life. I strongly believe that <b>Honesty is the best policy ever in the world</b> and <b>Courtest costs nothing but buys everything.</b> So my approach is to become a tech expert by working hard honestly.</p>
+            </div>
         </div>
     );
 };
