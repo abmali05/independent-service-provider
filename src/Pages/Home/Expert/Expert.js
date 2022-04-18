2906037b2818adb468a558c88e9a845d198e4542
abmali05 <bestofhonest@gmail.com>
2022-04-18 20:08:42 +0600

    expert updated

--- src/Pages/Home/Expert/Expert.js
+++ src/Pages/Home/Expert/Expert.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './Expert.css';
 
 const Expert = ({ expert }) => {
-    const { name, img } = expert;
+    const { name, desc } = expert;
     return (
-        <div className='service'>
+        <div className='service bg-light'>
 
-            <img src={img} className="w-100" alt="..." />
 
-            <h5>{name}</h5>
-            <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+            <h5 className='pt-3'>{name}</h5>
+            <p>{desc}</p>
             <a href="#" className="btn btn-secondary">Details</a>
 
 
--- src/Pages/Home/Experts/Experts.js
+++ src/Pages/Home/Experts/Experts.js
@@ -7,9 +7,9 @@ import Expert from '../Expert/Expert';
 import './Experts.css';
 
 const experts = [
-    { id: 1, name: 'Will Smith', img: expert1 },
-    { id: 2, name: 'Chris Rock', img: expert2 },
-    { id: 3, name: 'Dwayne Rock', img: expert3 },
+    { id: 1, name: 'Business Law', desc: 'I have enough experience on business law. I am working on it for the last 13 years.' },
+    { id: 2, name: 'Family Law', desc: 'Regarding family law, I have hands on practice for last 20 years. There are a lots of happy clients.' },
+    { id: 3, name: 'Civil Litigation', desc: 'This topic is a little bit complex. But I am very much familiar with its ins and outs.' },
 
 ]
 
